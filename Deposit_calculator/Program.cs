aab9fcc49309f372cc403ee59bb4eb5455f0e032
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-23 11:11:59 +0300

    deposit_calculator

---
+++ Deposit_calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿double depositSum = double.Parse(Console.ReadLine());
+int month = int.Parse(Console.ReadLine());
+double yearPercent = double.Parse(Console.ReadLine());
+
+double accredInterest = depositSum * (yearPercent / 100);
+double accredInterestPerMonth = accredInterest / 12;
+double resultSum = depositSum + month * accredInterestPerMonth;
+
+Console.WriteLine(resultSum);
