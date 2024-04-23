b9350850a84d202c941bd8510a0ea22a78271d2e
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-23 11:36:58 +0300

    day of week

---
+++ Day_of_week/Program.cs
@@ -0,0 +1,26 @@
+﻿int numDay = int.Parse(Console.ReadLine());
+
+switch (numDay)
+{
+    case 1:
+        Console.Write("Monday");
+        break;
+    case 2:
+        Console.Write("Tuesday");
+        break;
+    case 3:
+        Console.Write("Wednesday");
+        break;
+    case 4:
+        Console.Write("Thursday");
+        break;
+    case 5:
+        Console.Write("Friday");
+        break;
+    case 6:
+        Console.Write("Saturday");
+        break;
+    case 7:
+        Console.Write("Sunday");
+        break;
+}
