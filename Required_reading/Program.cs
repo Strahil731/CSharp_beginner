86cc3492cdd412f5f95bc5ded8abdfaa387a1c83
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-23 11:17:39 +0300

    required_reading

---
+++ Required_reading/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberAllPage = int.Parse(Console.ReadLine());
+int pageForOneHour = int.Parse(Console.ReadLine());
+int dayNumber = int.Parse(Console.ReadLine());
+
+int needHour = numberAllPage / pageForOneHour;
+int needHourPerDay = needHour / dayNumber;
+
+Console.WriteLine(needHourPerDay);
