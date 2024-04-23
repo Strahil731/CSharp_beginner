a26057e1bc243fde14cf4057604b6cffc768532a
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-23 10:55:55 +0300

    student info

---
+++ Student_information/Program.cs
@@ -0,0 +1,5 @@
+﻿string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hello! I'm {firstName} {lastName}, a {age} year old.");
